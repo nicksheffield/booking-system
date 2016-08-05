07a2b60e2dbe5a4f80b8f7cc36f6e1e139f983b0
Nick Sheffield <numbereft@gmail.com>
2016-08-05 18:24:41 +1200

    added user resource and removed login tests

---
+++ app/Http/Controllers/User.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+
+use App\User as Model;
+
+class User extends Controller
+{
+	public function index()
+	{
+		return Model::all();
+	}
+
+	/**
+	 * Store a newly created resource in storage.
+	 *
+	 * @param  \Illuminate\Http\Request  $request
+	 * @return \Illuminate\Http\Response
+	 */
+	public function store(Model $model, Request $request)
+	{
+		$model->fill($request->all());
+
+		$model->password = bcrypt($request->password);
+
+		$model->save();
+
+		return $model;
+	}
+
+	/**
+	 * Display the specified resource.
+	 *
+	 * @param  int  $id
+	 * @return \Illuminate\Http\Response
+	 */
+	public function show($id)
+	{
+		return Model::find($id);
+	}
+
+	/**
+	 * Update the specified resource in storage.
+	 *
+	 * @param  \Illuminate\Http\Request  $request
+	 * @param  int  $id
+	 * @return \Illuminate\Http\Response
+	 */
+	public function update(Request $request, $id)
+	{
+		$model = Model::find($id);
+		
+		$model->fill($request->except('password'));
+
+		if($request->password) {
+			$model->password = bcrypt($request->password);
+		}
+		
+		$model->save();
+
+		return $model;
+	}
+
+	/**
+	 * Remove the specified resource from storage.
+	 *
+	 * @param  int  $id
+	 * @return \Illuminate\Http\Response
+	 */
+	public function destroy($id)
+	{
+		$model = Model::find($id);
+
+		$model->delete();
+
+		return $model;
+	}
+}
--- app/User.php
+++ app/User.php
@@ -4,8 +4,13 @@ namespace App;
 
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
 class User extends Authenticatable
 {
+    use SoftDeletes;
+    
     /**
      * The attributes that are mass assignable.
      *
