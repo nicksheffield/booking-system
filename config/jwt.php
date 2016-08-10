02337719a56ffc06beae9ac9affe9b6c23d64f3e
Nick Sheffield <numbereft@gmail.com>
2016-08-10 15:04:36 +1200

    added jwt secret

--- config/jwt.php
+++ config/jwt.php
@@ -21,7 +21,7 @@ return [
     |
     */
 
-    'secret' => env('JWT_SECRET', 'changeme'),
+    'secret' => env('JWT_SECRET', 'VF9Y3exIVyJX2uzCEGGK0ZeBNU0IJiiz'),
 
     /*
     |--------------------------------------------------------------------------
