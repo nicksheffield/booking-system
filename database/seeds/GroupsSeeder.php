b8963fac307e6c1dad0144b33620680a169aab38
Nick Sheffield <numbereft@gmail.com>
2017-03-08 17:17:02 +1300

    fixed duplicate groups being seeded

--- database/seeds/GroupsSeeder.php
+++ database/seeds/GroupsSeeder.php
@@ -39,13 +39,21 @@ class GroupsSeeder extends Seeder
 		$months = ['02', '04', '07', '09'];
 		
 		$extra_classes = 20;
+
+		$codes = ['16ADDAV07', '16ADGD02B', '16ADIC09'];
 		
 		for($i=0; $i<$extra_classes; $i++) {
-			$type = mt_rand(0, count($types) - 1);
-			$month = mt_rand(0, count($months) - 1);
+			do {
+				$type = mt_rand(0, count($types) - 1);
+				$month = mt_rand(0, count($months) - 1);
+
+				$code = '16AD' . $types[$type]->code . $months[$month];
+			} while(array_search($code, $codes) !== false);
+
+			$codes[] = $code;
 			$items[] = [
 				'id' => $items[count($items)-1]['id'] + 1,
-				'code' => '16AD' . $types[$type]->code . $months[$month],
+				'code' => $code,
 				'group_type_id' => $types[$type]->id,
 				'enrollment_key' => 'blah'
 			];
