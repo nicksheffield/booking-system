b018929db0c8db852920457c12bd018ff17d199b
Nick Sheffield <numbereft@gmail.com>
2017-03-16 10:41:42 +1300

    implemented filter

--- angular/components/booking/manage/bookingsManageCtrl.js
+++ angular/components/booking/manage/bookingsManageCtrl.js
@@ -1,7 +1,7 @@
 angular.module('app.controllers')
 
-.controller('bookingsManageCtrl', function($scope, $store, $load, $merge, $stateParams, $http, $location, Booking, $prepare, $bookingFilter, $window, $queryString) {
-	window.scope = $scope
+.controller('bookingsManageCtrl', function($scope, $store, $load, $merge, $stateParams, $http, $location, Booking, $prepare, $window, $queryString) {
+	window.$scope = $scope
 
 	$scope.users = $store.users
 	$scope.groups = $store.groups
@@ -12,7 +12,6 @@ angular.module('app.controllers')
 
 		bookings.$promise.then(function(res) {
 			_.forEach(res, (a) => $scope.bookings.push(a))
-			console.log('loaded', $scope.bookings.length)
 
 			if(res.length == 100) {
 				loadBookings(page + 1)
@@ -25,7 +24,6 @@ angular.module('app.controllers')
 	loadBookings(1)
 
 	function simplifyBookings() {
-		console.log('simplified')
 		$store.bookings = $scope.bookings
 
 		$scope.bookings = []
@@ -40,11 +38,14 @@ angular.module('app.controllers')
 					}
 				},
 				closed_at: booking.closed_at,
+				created_at: booking.created_at,
 				_status: booking._status,
 				_overdue: booking._overdue,
 				_priority: booking._priority
 			})
 		})
+		
+		$scope.advFilter.apply()
 	}
 
 	// --------------------------------------------------------------------------------
@@ -65,5 +66,98 @@ angular.module('app.controllers')
 		$scope.openBeforeDateControl = $scope.openBeforeDateControl ? false : true
 		$scope.filterUseBeforeDate = true
 	}
+
+	// --------------------------------------------------------------------------------
+	// Filter
+	// --------------------------------------------------------------------------------
+
+	$scope.advFilter = {
+		open: false,
+		props: {},
+		defaults: {
+			after: null,
+			before: null,
+			user: null,
+			overdue: true,
+			closed: false,
+			issued: true,
+			booked: true,
+			limit: 10
+		},
+
+		toggleOpen: function() {
+			$scope.advFilter.open = !$scope.advFilter.open
+		},
+		clear: function() {
+			$scope.advFilter.props = _.clone($scope.advFilter.defaults)
+			$scope.advFilter.apply()
+		},
+		clearUser: function() {
+			$scope.advFilter.props.user = null
+		},
+		apply: function() {
+			if($scope.advFilter.props.limit === 0) return
+
+			var temp = [], temp2 = []
+			
+			_.forEach($store.bookings, function(booking) {
+				var keep = true
+
+				if($scope.advFilter.props.after) {
+					if(booking.created_at.valueOf() < $scope.advFilter.props.after.valueOf()) {
+						keep = false
+					}
+				}
+			
+				if($scope.advFilter.props.before) {
+					if(booking.created_at.valueOf() > $scope.advFilter.props.before.valueOf()) {
+						keep = false
+					}
+				}
+
+				if($scope.advFilter.props.user) {
+					if(booking.user.id !== $scope.advFilter.props.user.id) {
+						keep = false
+					}
+				}
+
+				if(keep) temp.push(booking)
+			})
+
+			_.forEach(temp, function(booking) {
+				var keep = false
+
+				if($scope.advFilter.props.closed) {
+					if(booking.closed_at) {
+						keep = true
+					}
+				}
+
+				if($scope.advFilter.props.issued) {
+					if(booking.taken_at && !booking.closed_at && !booking._overdue) {
+						keep = true
+					}
+				}
+
+				if($scope.advFilter.props.overdue) {
+					if(booking.taken_at && booking._overdue && !booking.closed_at) {
+						keep = true
+					}
+				}
+
+				if($scope.advFilter.props.booked) {
+					if(!booking.taken_at) {
+						keep = true
+					}
+				}
+
+				if(keep) temp2.push(booking)
+			})
+
+			$scope.bookings = temp2
+		}
+	}
+
+	$scope.advFilter.clear()
 
 })
