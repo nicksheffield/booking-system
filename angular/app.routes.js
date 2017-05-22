9b0bfc1a76d04f0b5891e0d933528cad67480477
Nick Sheffield <numbereft@gmail.com>
2017-05-22 15:13:58 +1200

    injected

--- angular/app.routes.js
+++ angular/app.routes.js
@@ -1,6 +1,6 @@
 angular.module('app.routes')
 
-.config(function($stateProvider, $urlRouterProvider) {
+.config(function($stateProvider, $urlRouterProvider, $locationProvider) {
 
 	$locationProvider.hashPrefix('')
 
