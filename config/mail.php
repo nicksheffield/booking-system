c45085e1e981592b965c053635946e1c1ddfb5ea
Nick Sheffield <numbereft@gmail.com>
2017-05-30 19:47:14 +1200

    set default sender

--- config/mail.php
+++ config/mail.php
@@ -55,7 +55,7 @@ return [
     |
     */
 
-    'from' => ['address' => null, 'name' => null],
+    'from' => ['address' => 'hello@myApp.com', 'name' => 'Booking System'],
 
     /*
     |--------------------------------------------------------------------------
