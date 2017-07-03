07c6e38a799ac146b32032fc8a6c3d8456b1e423
Nick Sheffield <numbereft@gmail.com>
2017-07-03 13:50:41 +1200

    fix for unit page being broken

--- angular/components/manage/units/controllers/unitManageCtrl.js
+++ angular/components/manage/units/controllers/unitManageCtrl.js
@@ -3,6 +3,6 @@ angular.module('app.controllers')
 .controller('unitManageCtrl', function($scope, $store) {
 	$scope.units = $store.units
 
-	$scope.product_id  = u => u.product.id
+	$scope.product_id  = u => u.product ? u.product.id : 0
 	$scope.unit_number = u => u.unit_number
 })
