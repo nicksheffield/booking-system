53f007d1f4117a98d77a705d3ee3baf1b5b6f8ac
Nick Sheffield <numbereft@gmail.com>
2017-07-10 16:39:15 +1200

    fixed loading booking problem

--- angular/pages/booking/view/viewBookingCtrl.js
+++ angular/pages/booking/view/viewBookingCtrl.js
@@ -1,7 +1,6 @@
 angular.module('app.controllers')
 
 .controller('viewBookingCtrl', function($scope, $stateParams, $store, $location, $http, $invalidate, Booking, $prepare, $merge) {
-	$scope.booking = $store.get('bookings', $stateParams.id)
 	$scope.user = $store.user
 	$scope.errors = []
 
@@ -11,22 +10,17 @@ angular.module('app.controllers')
 		return $store.get('units', id)
 	}
 	
-	if($scope.booking) {
-		console.log('not loaded')
-		$scope.return_difference = moment().diff(new Date($scope.booking.closed_at), 'hours')
-	} else {
-		console.log('loaded')
-		$scope.booking = Booking.get({id: $stateParams.id, with: 'user|products', token: localStorage.satellizer_token})
-		
-		$scope.booking.$promise
-			.then($prepare.booking)
-			.then(function(booking) {
-				$merge.bookings([booking])
-				$scope.return_difference = moment().diff(new Date($scope.booking.closed_at), 'hours')
-			}, function(err) {
-				$scope.errors.push({message: err.data.error})
-			})
-	}
+	$scope.booking = Booking.get({id: $stateParams.id, with: 'user|products', token: localStorage.satellizer_token})
+	
+	$scope.booking.$promise
+		.then($prepare.booking)
+		.then(function(booking) {
+			$merge.bookings([booking])
+			$scope.return_difference = moment().diff(new Date($scope.booking.closed_at), 'hours')
+		}, function(err) {
+			$scope.errors.push({message: err.data.error})
+		})
+
 
 	$scope.delete = function() {
 		var confirmed = confirm('Are you sure you want to delete this?')
